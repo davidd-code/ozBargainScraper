d6abad26cf84e597849e5d9e629b31cd1dd2cc0e
davidd11 <ddoan.0411@gmail.com>
2019-01-17 22:26:30 +1100

    update index route

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started");
-})
+});
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ var express = require('express'),
     router  = express.Router();
 
 router.get("/", function(req, res){
-    res.send("This is the index page");    
+    res.send("This is the index page");
 });
+
+module.exports = router;
