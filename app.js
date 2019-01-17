7491e5b82d343809fd40956778aa64ca84deeb5a
davidd11 <ddoan.0411@gmail.com>
2019-01-17 21:24:08 +1100

    scrape titles from ozbargain

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const rp = require('request-promise');
+var rp      = require('request-promise'),
+    cheerio = require('cheerio');
+
 const url = 'https://ozbargain.com.au';
 
 rp(url)
   .then(function(html){
     //success
-    console.log(html);
+    //create array to hold titles
+    var dataTitles = [];
+    //loop through all of the titles and push them to the array
+    for(let i = 0; i < 30; i++) {
+      dataTitles.push(cheerio('h2[class="title"]', html)[i].attribs['data-title']);
+    }
+    console.log(dataTitles);
   })
   .catch(function(err){
     console.log(err);
