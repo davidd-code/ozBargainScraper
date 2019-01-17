712bac3447c94da34f8cfb4bbe89fd8e6989d186
davidd11 <ddoan.0411@gmail.com>
2019-01-17 22:08:02 +1100

    set up app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app     = express();
+
+//routes
+var indexRoutes = require('./routes/index.js');
+
+app.set("view engine", "ejs");
+
+app.use("/", indexRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp server has started");
+})
